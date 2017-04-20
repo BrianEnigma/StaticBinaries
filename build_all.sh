7afa0f1f984045080d17dbbba0f6f01674c086d6
Brian Enigma <brian@netninja.com>
2017-04-19 18:28:07 -0700

    Skeleton script to build everything

---
+++ build_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build tool prerequisites
+
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install yasm
+
+# Static libraries
+
+static_libpng_build.sh
+static_libjpeg_build.sh
+
+# Static applications
+
+#TODO
+
