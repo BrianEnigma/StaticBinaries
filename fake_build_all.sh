a65edaf1958b64216e26908980256e00dce6d872
Brian Enigma <brian@netninja.com>
2017-04-21 17:24:51 -0700

    More fixup

--- fake_build_all.sh
+++ fake_build_all.sh
@@ -8,6 +8,7 @@ BINS="ffmpeg ffprobe ffserver sox magick mediainfo MP4Box MP42TS MP4Client"
 
 for f in $BINS; do
     echo "Fake-creating: bin/$f"
+    mkdir -p bin
     touch bin/$f
     sleep 5
 done
